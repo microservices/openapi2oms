0f7bcaf8f46f877345ec4a87847a26c9f5013072
Jude Pereira <contact@judepereira.com>
2019-08-12 16:35:01 +0200

    Implement an action name generator, given a URL.

--- openapi2omg/utils/OpenAPIActionUtil.py
+++ openapi2omg/utils/OpenAPIActionUtil.py
@@ -1,13 +1,40 @@
 # -*- coding: utf-8 -*-
+import re
+
 from openapi2omg.exceptions.MappingError import MappingError
 
 
 class OpenAPIActionUtil:
+    PATTERN_WORD = re.compile(r'[a-z{}A-Z0-9]*')
+    PATTERN_SANITIZE = re.compile(r'((?![a-zA-Z0-9]).)')
 
     @staticmethod
     def _gen_action_name(path: str, http_method: str, content: dict):
-        # TODO: impl me
-        raise Exception('todo')
+        res = OpenAPIActionUtil.PATTERN_WORD.finditer(path)
+        action_name = http_method
+        first_param_encountered = False
+        for part in res:
+            word = part.group()
+            if word == '':
+                continue
+
+            if word.startswith('{'):
+                ext = 'By'
+                if first_param_encountered:
+                    ext = 'And'
+
+                append = ext + word[1:-1].capitalize()
+                first_param_encountered = True
+            else:
+                append = word.capitalize()
+
+            action_name += OpenAPIActionUtil._sanitize(append)
+
+        return action_name
+
+    @staticmethod
+    def _sanitize(string: str) -> str:
+        return re.sub(OpenAPIActionUtil.PATTERN_SANITIZE, '', string)
 
     @staticmethod
     def generate_action_name(path: str, http_method: str, content: dict):
